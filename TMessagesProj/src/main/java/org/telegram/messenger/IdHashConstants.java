f156e3470bdb50f64868191c1f6831b84eda169e
jzinferno <jzinferno@gnail.com>
2025-03-10 20:40:40 +0200

    FirstTest

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/IdHashConstants.java
@@ -0,0 +1,6 @@
+package org.telegram.messenger;
+
+public class IdHashConstants {
+    public static final String APP_HASH = "ab799a7ba264787e9fb7dcdad719c092";
+    public static final int APP_ID = 17036621;
+}
